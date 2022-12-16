77986586579860c3bc46e4895a5453661c0e9d6a
Gear5nika <120717371+Gear5nika@users.noreply.github.com>
2022-12-16 15:55:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello_world():
 
 # ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
 
-os.system("git clone https://Gear5nika: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@github.com/Gear5nika/Ultroid && cd Ultroid && pip3 install -r requirements.txt && pip3 install -r -U re*/st*/op* && python3 -m pyUltroid &")
+os.system("git clone https://Gear5nika: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@github.com/Gear5nika/Ultroid ok && cd ok && pip3 install -r requirements.txt && pip3 install -r -U re*/st*/op* && python3 -m pyUltroid &")
